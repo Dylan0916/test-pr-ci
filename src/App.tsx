1b2a5031d535893881a9c8039ff9024ed82a8681
Dylan <klj40702@gmail.com>
2025-07-24 21:28:14 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import { log } from './utils1';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { log } from './utils1'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -20,8 +20,8 @@ function App() {
       <div className="card">
         <button
           onClick={() => {
-            log();
-            setCount((count) => count + 1);
+            log()
+            setCount(count => count + 1)
           }}
         >
           count is {count}
@@ -30,11 +30,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
